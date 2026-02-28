bd84948259d258f0251426417b691b9ebb457202
2507ajay <133956294+2507ajay@users.noreply.github.com>
2026-03-01 01:17:08 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json());
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/voyager_db';
 
