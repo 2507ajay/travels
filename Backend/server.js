74f8523d60954458967b6b08eedc8c2c35ca1c7f
2507ajay <133956294+2507ajay@users.noreply.github.com>
2026-03-01 00:59:36 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/voyager_db
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected Successfully'))
-  .catch((err) => console.error('❌ MongoDB Connection Error:', err));
+  .catch((err) => console.error('❌ MongoDB Connection Error:', err)); 
 
 // --- 1. SCHEMAS & MODELS ---
 
