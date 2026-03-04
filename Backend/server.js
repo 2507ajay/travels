796f7c288855a1b0f31513c82cb4d263981b1ef4
2507ajay <133956294+2507ajay@users.noreply.github.com>
2026-03-04 11:19:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').Backend();
 const connectDB = require('./db');
 
 const app = express();
